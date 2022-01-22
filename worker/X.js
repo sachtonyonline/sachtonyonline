0b99a458f81ff0068ac730d30426f195365b451a
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2022-01-22 16:07:28 +0700

    Update X.js

--- worker/X.js
+++ worker/X.js
@@ -1,10 +1,29 @@
+if (typeof(Worker) !== "undefined") {
+  // Yes! Web worker support!
+  // Some code.....
+} else {
+  // Sorry! No Web Worker support..
+};
+
 var i = "Balance";
 var X = "Amount";
 
 function balance() {
   i = i + X;
   postMessage(i);
-  setbalance("balance()",500);
+  setbalance("balance()",1000);
 }
 
 balance();
+
+if (typeof(w) == "undefined") {
+  w = new Worker("X.js");
+}
+
+w.onmessage = function(event){
+  document.getElementById("result").innerHTML = event.data;
+};
+
+w.terminate();
+
+w = undefined;
