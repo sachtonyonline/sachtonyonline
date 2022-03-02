da0b509db1b1a55d3c6e2e52e80c9b00fd50bdd9
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2022-03-02 17:54:21 +0700

    Update angular_include.php

--- angular_include.php
+++ angular_include.php
@@ -1,13 +1,26 @@
 <?php
-echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
+  echo $_SERVER['PHP_SELF'];
+  echo "<br>";
+  echo $_SERVER['SERVER_NAME'];
+  echo "<br>";
+  echo $_SERVER['HTTP_HOST'];
+  echo "<br>";
+  echo $_SERVER['HTTP_REFERER'];
+  echo "<br>";
+  echo $_SERVER['HTTP_USER_AGENT'];
+  echo "<br>";
+  echo $_SERVER['SCRIPT_NAME'];
+
+  $servername = "localhost";
+  $username = "username";
+  $password = "password";
+
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
 ?>
